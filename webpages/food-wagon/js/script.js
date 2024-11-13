5fbbdeee0a31739e7a22c7fc2ff8aabf75cd1a53
eugene-lo <john_h@list.ru>
2024-11-13 16:29:06 +0300

    добавлен food-wagon

---
+++ webpages/food-wagon/js/script.js
@@ -0,0 +1,42 @@
+var swiper = new Swiper(".swiper", {
+	slidesPerView: 4,
+	spaceBetween: 25,
+	loop: true,
+	centerSlide: 'true',
+	fade: 'true',
+	grabCursor: 'true',
+	pagination: {
+	el: ".swiper-pagination",
+	clickable: true,
+	dynamicBullets: true,
+	},
+	navigation: {
+	nextEl: ".swiper-button-next",
+	prevEl: ".swiper-button-prev",
+	},
+  
+	breakpoints:{
+	320: {
+	slidesPerView: 1,
+	},
+	420: {
+	slidesPerView: 2,
+	},
+	520: {
+	slidesPerView: 2,
+	},
+	660: {
+		slidesPerView: 3,
+	},
+	850: {
+		slidesPerView: 3,
+	},
+	960: {
+	slidesPerView: 4,
+	},
+	1400: {
+	slidesPerView: 4,
+	}
+
+	},
+	});
