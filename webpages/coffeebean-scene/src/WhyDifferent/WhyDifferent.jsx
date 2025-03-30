2a208329f7a715db72b964a3dce4d815ff17c9bd
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-03-31 00:08:25 +0300

    add mapping for cards

--- webpages/coffeebean-scene/src/WhyDifferent/WhyDifferent.jsx
+++ webpages/coffeebean-scene/src/WhyDifferent/WhyDifferent.jsx
@@ -1,5 +1,31 @@
 function WhyDifferent () {
+
+    const advantages = [
+        {
+            image: "src/assets/img/supreme-beans.webp",
+            title: "Supreme Beans",
+            description: "Beans that provides great taste",
+        },
+        {
+            image: "src/assets/img/high-quality.webp",
+            title: "High Quality",
+            description: "We provide the highest quality",
+        },
+        {
+            image: "src/assets/img/extraordinary.webp",
+            title: "Extraordinary ",
+            description: "Coffee like you have never tasted",
+        },
+        {
+            image: "src/assets/img/affordable-price.webp",
+            title: "Affordable Price",
+            description: "Our Coffee prices are easy to afford",
+        }
+    ];
+
+
     return (
+
         <div className="why-different">
 
             <div className="why-different-wrapper">
@@ -7,10 +33,15 @@ function WhyDifferent () {
                 <div className="why-different__subtitle">We don’t just make your coffee, we make your day!</div>
 
                 <div className="why-different-advantages">
-                    <div className="why-different-advantage"></div>
-                    <div className="why-different-advantage"></div>
-                    <div className="why-different-advantage"></div>
-                    <div className="why-different-advantage"></div>
+                    {advantages.map((item) => (
+                        <div key={item.title}>
+                            <div className="why-different-advantage__image">
+                                <img src={item.image} alt="" />
+                                </div>
+                            <div className="why-different-advantage__title">{item.title}</div>
+                            <div className="why-different-advantage__description">{item.description}</div>
+                        </div>
+                    ))}
                 </div>
             </div>
 
