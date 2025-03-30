a2cf0deb0b1f8dc34a4466c19b8f96810732adf9
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-03-30 22:25:42 +0300

    change button position

--- webpages/coffeebean-scene/src/BlendList/BlendList.jsx
+++ webpages/coffeebean-scene/src/BlendList/BlendList.jsx
@@ -40,23 +40,34 @@ function BlendList() {
     return (
         <div className="blend">
             <div className="blend-wrapper">
-                <div className="blend__title">Enjoy a new blend of coffee style</div>
+                <div className="blend__title">
+                    Enjoy a new blend of coffee style
+                </div>
                 <div className="blend__subtitle">
-                    Explore all flavours of coffee with us. There is always a new cup worth experiencing
+                    Explore all flavours of coffee with us. There is always a
+                    new cup worth experiencing
                 </div>
                 <div className="blend-cards">
-                    {blends.map((item)=>(
-                            <div className="blend-card" key={item.id}>
-                                <span className="blend-card__image">
-                                    <img src={item.image} alt="" />
-                                </span>
-                                <div className="blend-card__body">
-                                    <div className="blend-card__title">{item.name}</div>
-                                    <div className="blend-card__proportions">{item.proportions}</div>
-                                    <div className="blend-card__price">{item.price}</div>
-                                    <div className="blend-card__orderButton">{item.orderButton}</div>
+                    {blends.map((item) => (
+                        <div className="blend-card" key={item.id}>
+                            <span className="blend-card__image">
+                                <img src={item.image} alt="" />
+                            </span>
+                            <div className="blend-card__body">
+                                <div className="blend-card__title">
+                                    {item.name}
+                                </div>
+                                <div className="blend-card__proportions">
+                                    {item.proportions}
+                                </div>
+                                <div className="blend-card__price">
+                                    {item.price}
+                                </div>
+                                <div className="blend-card__orderButton">
+                                    {item.orderButton}
                                 </div>
                             </div>
+                        </div>
                     ))}
                 </div>
             </div>
