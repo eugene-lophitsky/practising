d09a4faceb369e6585d3729eaed16377828e2213
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-03-31 23:15:40 +0300

    add background to cards

--- webpages/coffeebean-scene/src/WhyDifferent/WhyDifferent.jsx
+++ webpages/coffeebean-scene/src/WhyDifferent/WhyDifferent.jsx
@@ -2,22 +2,22 @@ function WhyDifferent () {
 
     const advantages = [
         {
-            image: "src/assets/img/supreme-beans.webp",
+            image: "src/assets/img/supreme-beans.png",
             title: "Supreme Beans",
             description: "Beans that provides great taste",
         },
         {
-            image: "src/assets/img/high-quality.webp",
+            image: "src/assets/img/high-quality.png",
             title: "High Quality",
             description: "We provide the highest quality",
         },
         {
-            image: "src/assets/img/extraordinary.webp",
+            image: "src/assets/img/extraordinary.png",
             title: "Extraordinary ",
             description: "Coffee like you have never tasted",
         },
         {
-            image: "src/assets/img/affordable-price.webp",
+            image: "src/assets/img/affordable-price.png",
             title: "Affordable Price",
             description: "Our Coffee prices are easy to afford",
         }
@@ -34,7 +34,7 @@ function WhyDifferent () {
 
                 <div className="why-different-advantages">
                     {advantages.map((item) => (
-                        <div key={item.title}>
+                        <div key={item.title} className="why-different-advantage">
                             <div className="why-different-advantage__image">
                                 <img src={item.image} alt="" />
                                 </div>
