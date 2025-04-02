f1c354e39e653e693859066fbf444f07e6633621
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-02 22:49:00 +0300

    set structure

--- webpages/coffeebean-scene/src/AmazingMorning/AmazingMorning.jsx
+++ webpages/coffeebean-scene/src/AmazingMorning/AmazingMorning.jsx
@@ -2,7 +2,8 @@ function AmazingMorning () {
     return (
         <div className="amazing-morning">
             <div className="amazing-morning-wrapper">
-                
+                <div className="amazing-morning-detailed"></div>
+                <div className="amazing-morning__image"></div>
             </div>
         </div>
     )
