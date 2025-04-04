00388e623d86cb0a762086df512ab8df3ff6294b
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-04 22:56:34 +0300

    colorize body

--- webpages/coffeebean-scene/src/Feedback/Feedback.jsx
+++ webpages/coffeebean-scene/src/Feedback/Feedback.jsx
@@ -5,7 +5,21 @@ function Feedback () {
                 <div className="feedback__title">Our coffee perfection feedback</div>
                 <div className="feedback__subtitle">Our customers has amazing things to say about us</div>
 
-                <div className="feedback-body"></div>
+                <div className="feedback-body">
+                    <div className="feedback-body__quote">
+                    Lorem Ipsum is simply dummy text of the
+                    printing and typesetting industry. Lorem
+                    Ipsum has been the industry's standard 
+                    dummy text ever since the 1500s, when 
+                    an unknown printer took a galley of type
+                    and scrambled it to make a type specimen
+                    book. It has survived not only five 
+                    centuries, but also the leap into 
+                    electronic typesetting, remaining essentially
+                    unchanged. It was popularised in the 1960s
+                     with the release of Letraset.....
+                    </div>
+                </div>
 
             </div>
         </div>
