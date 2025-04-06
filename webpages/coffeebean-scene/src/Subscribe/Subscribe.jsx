db922a1c6ff182d113e60823e11186e61e36c757
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-06 21:44:11 +0300

    stylize text

--- webpages/coffeebean-scene/src/Subscribe/Subscribe.jsx
+++ webpages/coffeebean-scene/src/Subscribe/Subscribe.jsx
@@ -1,7 +1,10 @@
 function Subscribe () {
     return (
         <div className="subscribe">
-        
+            <div className="subscribe-wrapper">
+                <div className="subscribe__title">Subscribe to get the Latest News</div>
+                <div className="subscribe__subtitle">Don’t miss out on our latest news, updates, tips and special offers</div>
+            </div>
         </div>
     )
 }
