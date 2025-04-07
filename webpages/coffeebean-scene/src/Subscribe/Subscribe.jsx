dc84bbb666d0f0d63f644e3ffa97ff7018af6321
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-07 22:01:19 +0300

    add from elements

--- webpages/coffeebean-scene/src/Subscribe/Subscribe.jsx
+++ webpages/coffeebean-scene/src/Subscribe/Subscribe.jsx
@@ -4,6 +4,10 @@ function Subscribe () {
             <div className="subscribe-wrapper">
                 <div className="subscribe__title">Subscribe to get the Latest News</div>
                 <div className="subscribe__subtitle">Don’t miss out on our latest news, updates, tips and special offers</div>
+                <div className="subscribe__form">
+                    <input type="text" className="subscribe__form-input" />
+                    <button className="subscribe__form-button">Subscribe</button>
+                </div>
             </div>
         </div>
     )
