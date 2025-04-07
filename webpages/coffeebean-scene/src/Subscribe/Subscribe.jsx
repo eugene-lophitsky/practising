7ecb2b50dc45333b341d2effa9ce6cd6a4a870a1
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-07 22:42:31 +0300

    initialize footer

--- webpages/coffeebean-scene/src/Subscribe/Subscribe.jsx
+++ webpages/coffeebean-scene/src/Subscribe/Subscribe.jsx
@@ -1,16 +1,23 @@
-function Subscribe () {
+function Subscribe() {
     return (
         <div className="subscribe">
             <div className="subscribe-wrapper">
-                <div className="subscribe__title">Subscribe to get the Latest News</div>
-                <div className="subscribe__subtitle">Don’t miss out on our latest news, updates, tips and special offers</div>
+                <div className="subscribe__title">
+                    Subscribe to get the Latest News
+                </div>
+                <div className="subscribe__subtitle">
+                    Don’t miss out on our latest news, updates, tips and special
+                    offers
+                </div>
                 <div className="subscribe__form">
                     <input type="text" className="subscribe__form-input" />
-                    <button className="subscribe__form-button">Subscribe</button>
+                    <button className="subscribe__form-button">
+                        Subscribe
+                    </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Subscribe;
