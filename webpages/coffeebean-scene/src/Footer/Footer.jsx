ff3ecf580aa86a6c012b774c9c7277d18e8880ca
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-15 19:53:56 +0300

    add paddings

--- webpages/coffeebean-scene/src/Footer/Footer.jsx
+++ webpages/coffeebean-scene/src/Footer/Footer.jsx
@@ -12,7 +12,7 @@ function Footer () {
                     <div className="footer-common__description">
                     Lorem Ipsum is simply dummy text of the 
                     printing and typesetting industry. Lorem
-                     Ipsum has been the industry's standard 
+                     Ipsum has been the industry&apos;s standard 
                      dummy text ever since the 1500s, when 
                      an unknown printer took a galley of 
                      type and scrambled it to make a type
