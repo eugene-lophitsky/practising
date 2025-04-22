c44812543b92977dec38cd795a90a566f882b826
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-22 22:59:23 +0300

    add header widg. icons

--- webpages/electronics-store/src/components/HeaderWidgets/HeaderWidgets.jsx
+++ webpages/electronics-store/src/components/HeaderWidgets/HeaderWidgets.jsx
@@ -1,8 +1,22 @@
-
+import searchLens from '../../icons/search-form-lens.webp';
+import addToFavorites from '../../icons/add-to-favorites.svg';
+import addToCart from '../../icons/add-to-cart.svg';
 
 function HeaderWidgets () {
   return (
-    <div className="header-widgets">HeaderWidgets</div>
+
+    <div className="header-widgets">
+        <form className="header-widgets__search">
+            <input className="header-widgets__search-input" type="text" placeholder="What are you looking for?" />
+            <a href="#" className="header-widgets__search-button"><img src={searchLens} alt="" /></a>
+        </form>
+        <span className="header-widgets__shopping">
+          <a href="" className="header-widgets__shopping-favorites"><img src={addToFavorites} alt=""/></a>
+          <a href="" className="header-widgets__shopping-cart"><img src={addToCart} alt="" /></a>
+        </span>
+    </div>
+
+    
   )
 }
 
