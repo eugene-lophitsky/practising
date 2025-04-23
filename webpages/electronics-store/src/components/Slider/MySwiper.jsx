808c2f2de4d6a5dd9791debe0c6219333d0922f3
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-23 21:52:07 +0300

    add text to slider

--- webpages/electronics-store/src/components/Slider/MySwiper.jsx
+++ webpages/electronics-store/src/components/Slider/MySwiper.jsx
@@ -4,12 +4,14 @@ import './styles.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import firstSlide from '/jumbotron_slider_img_1.webp'
-import secondSlide from '/jumbotron_slider_img_2.webp'
-import thirdSlide from '/jumbotron_slider_img_3.webp'
+import firstSlide from '/jumbotron_slider_img_1.webp';
+import secondSlide from '/jumbotron_slider_img_2.webp';
+import thirdSlide from '/jumbotron_slider_img_3.webp';
+import appleLogo from '../../icons/slider-apple-logo.png';
 
 function MySwiper() {
   return (
+   <div className="swiper-wrapper">
     <Swiper
       modules={[Pagination]}
       spaceBetween={50}
@@ -22,6 +24,14 @@ function MySwiper() {
       <SwiperSlide><img src={secondSlide} alt="" /></SwiperSlide>
       <SwiperSlide><img src={thirdSlide} alt="" /></SwiperSlide>
     </Swiper>
+    <div className="swiper-advertising">
+      <div className="swiper-advertising__logo">
+        <img src={appleLogo} alt="" />
+        <span className="swiper-advertising__model">iPhone 14 Series</span>
+      </div>
+      <div className="swiper-advertising__text">Up to 10% <div>off Voucher</div></div>
+    </div>
+   </div>
   );
 }
 
