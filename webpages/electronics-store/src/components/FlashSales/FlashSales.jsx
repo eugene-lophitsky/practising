78546f721a1f82367b43c0a5179455eacdcf152c
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-24 20:36:19 +0300

    add slider

--- webpages/electronics-store/src/components/FlashSales/FlashSales.jsx
+++ webpages/electronics-store/src/components/FlashSales/FlashSales.jsx
@@ -1,12 +1,15 @@
+import FlashsalesSlider from "./FlashsalesSlider";
+
 function FlashSales () {
   return (
     <div className="flash-sales">
         <div className="flash-sales__wrapper">
           <div className="flash-sales__caption">
             <div className="flash-sales__caption-tab"></div>
-            <div className="flash-sales__caption-text">Today's</div>
+            <div className="flash-sales__caption-text">Today&apos;s</div>
           </div>
           <div className="flash-sales__title">Flash Sales</div>
+          <FlashsalesSlider/>
         </div>
     </div>
   )
---
+++ webpages/electronics-store/src/components/FlashSales/FlashsalesSlider.jsx
@@ -0,0 +1,37 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {Navigation} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import sliderImg1 from '/flashsales_slider_img1.webp';
+import sliderImg2 from '/flashsales_slider_img2.webp';
+import sliderImg3 from '/flashsales_slider_img3.webp';
+import sliderImg4 from '/flashsales_slider_img4.webp';
+
+function FlashsalesSlider() {
+  return (
+   <div className="swiper-wrapper">
+    <Swiper
+      modules={[Navigation]}
+      spaceBetween={50}
+      slidesPerView={4}
+      navigation
+      loop={true}
+      scrollbar={{ draggable: true }}
+      >
+      <SwiperSlide>
+        <div className="flash-sales-slide"><img src={sliderImg1} alt="" /></div>
+        
+      </SwiperSlide>
+      <SwiperSlide><img src={sliderImg2} alt="" /></SwiperSlide>
+      <SwiperSlide><img src={sliderImg3} alt="" /></SwiperSlide>
+      <SwiperSlide><img src={sliderImg4} alt="" /></SwiperSlide>
+      <SwiperSlide><img src={sliderImg3} alt="" /></SwiperSlide>
+      <SwiperSlide><img src={sliderImg4} alt="" /></SwiperSlide>
+    </Swiper>
+    
+   </div>
+  );
+}
+
+export default FlashsalesSlider;
