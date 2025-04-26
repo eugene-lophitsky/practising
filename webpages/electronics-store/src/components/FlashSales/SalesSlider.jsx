4a5e8233959d4779fb2e285511a85a0090bcc610
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-26 15:03:57 +0300

    add & stylize text

--- webpages/electronics-store/src/components/FlashSales/SalesSlider.jsx
+++ webpages/electronics-store/src/components/FlashSales/SalesSlider.jsx
@@ -6,6 +6,9 @@ import sliderImg1 from '/flashsales_slider_img1.webp';
 import sliderImg2 from '/flashsales_slider_img2.webp';
 import sliderImg3 from '/flashsales_slider_img3.webp';
 import sliderImg4 from '/flashsales_slider_img4.webp';
+import fourStar from '../../icons/four-star.svg';
+import fourHalfStar from '../../icons/four-half-star.svg';
+import fiveStar from '../../icons/five_star.svg';
 import '../FlashSales/FlashSales.css';
 
 function SalesSlider() {
@@ -24,34 +27,63 @@ function SalesSlider() {
         <SwiperSlide className="swiper-slide"> 
           <div className="swiper-slide__image"><img src={sliderImg1} alt="" /></div>
           <div className="swiper-slide__title">HAVIT HV-G92 Gamepad</div>
-          <div className="swiper-slide__price">P160</div>
-          <div className="swiper-slide__rating">wereweawa</div>
+          <div className="swiper-slide__price">
+              <div className="swiper-slide__price-new">$120</div>
+              <div className="swiper-slide__price-old">$160</div>
+          </div>
+          <div className="swiper-slide__rating">
+            <img src={fiveStar} alt="" />
+            <span className="swiper-slide__rating-count"></span>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">  
         <div className="swiper-slide__image"><img src={sliderImg2} alt="" /></div>
           <div className="swiper-slide__title">AK-900 Wired Keyboard</div>
-          <div className="swiper-slide__price">P160</div>
-          <div className="swiper-slide__rating">wereweawa</div>
+          <div className="swiper-slide__price">
+              <div className="swiper-slide__price-new">$960</div>
+              <div className="swiper-slide__price-old">$1160</div>
+          </div>
+          <div className="swiper-slide__rating">
+            <img src={fourStar} alt="" />
+            <span className="swiper-slide__rating-count"></span>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">  
         <div className="swiper-slide__image"><img src={sliderImg3} alt="" /></div>
           <div className="swiper-slide__title">IPS LCD Gaming Monitor</div>
-          <div className="swiper-slide__price">P160</div>
-          <div className="swiper-slide__rating">wereweawa</div>
+          <div className="swiper-slide__price">
+              <div className="swiper-slide__price-new">$370</div>
+              <div className="swiper-slide__price-old">$400</div>
+          </div>
+          <div className="swiper-slide__rating">
+            <img src={fiveStar} alt="" />
+            <span className="swiper-slide__rating-count"></span>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">  
         <div className="swiper-slide__image"><img src={sliderImg4} alt="" /></div>
-          <div className="swiper-slide__title">HAVIT HV-G92 Gamepad</div>
-          <div className="swiper-slide__price">P160</div>
-          <div className="swiper-slide__rating">wereweawa</div>
+          <div className="swiper-slide__title">S-Series Comfort Chair</div>
+          <div className="swiper-slide__price">
+              <div className="swiper-slide__price-new">$375</div>
+              <div className="swiper-slide__price-old">$400</div>
+          </div>
+          <div className="swiper-slide__rating">
+            <img src={fourHalfStar} alt="" />
+            <span className="swiper-slide__rating-count"></span>
+          </div>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">  
         <div className="swiper-slide__image"><img src={sliderImg3} alt="" /></div>
           <div className="swiper-slide__title">IPS LCD Gaming Monitor</div>
-          <div className="swiper-slide__price">P160</div>
-          <div className="swiper-slide__rating">wereweawa</div>
+          <div className="swiper-slide__price">
+              <div className="swiper-slide__price-new">$370</div>
+              <div className="swiper-slide__price-old">$400</div>
+          </div>
+          <div className="swiper-slide__rating">
+            <img src={fiveStar} alt="" />
+            <span className="swiper-slide__rating-count"></span>
+          </div>
         </SwiperSlide>
-
       </Swiper>
       <div className="swiper-navigation">
         <div className="custom-prev swiper-button-prev"></div>
@@ -62,3 +94,4 @@ function SalesSlider() {
 }
 
 export default SalesSlider;
+
