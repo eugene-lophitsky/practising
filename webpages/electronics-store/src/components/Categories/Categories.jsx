4b8586f593816d00644ee14a3c28cdd7dc0d4f27
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-28 14:55:01 +0300

    initiale 'categories'

---
+++ webpages/electronics-store/src/components/Categories/Categories.jsx
@@ -0,0 +1,13 @@
+
+
+function Categories () {
+  return (
+    <div className="categories">
+      <div className="categories-die"></div>
+      <div className="categories__title"></div>
+      <div className="categories-lis"></div>
+    </div>
+  )
+}
+
+export default Categories;
