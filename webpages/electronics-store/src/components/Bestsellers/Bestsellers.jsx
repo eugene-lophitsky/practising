5d2b46f286fc8f7fa52d0cc0742e3d943df7d4f1
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-04-29 21:52:40 +0300

    define block structure

--- webpages/electronics-store/src/components/Bestsellers/Bestsellers.jsx
+++ webpages/electronics-store/src/components/Bestsellers/Bestsellers.jsx
@@ -2,7 +2,14 @@ import styles from '../Bestsellers/Bestsellers.module.css';
 
 function Bestsellers () {
   return (
-    <div className="bestsellers"></div>
+    <div className={styles.bestsellers}>
+        <div className={styles.bestsellersDie}>
+            <div className={styles.bestsellersDieImg}></div>
+            <div className={styles.bestsellersDieText}></div>
+        </div>
+        <div className={styles.bestsellersTitle}></div>
+        <div className={styles.bestsellersList}></div>
+    </div>
   )
 }
 
