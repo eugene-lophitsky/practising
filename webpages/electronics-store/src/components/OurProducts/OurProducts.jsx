362e518ec1cb742ddc01f48592d060af0d9237a5
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-02 21:43:52 +0300

    add title

--- webpages/electronics-store/src/components/OurProducts/OurProducts.jsx
+++ webpages/electronics-store/src/components/OurProducts/OurProducts.jsx
@@ -3,11 +3,14 @@ import styles from "../OurProducts/OurProducts.module.css";
 function OurProducts() {
     return (
         <div className={styles.ourProducts}>
-            <div className="ourProductsWrapper">
-                <div className="ourProductsDie"></div>
-                <div className="ouProductsTitle"></div>
-                <div className="ourProductsList">
-                    <div className="ourProductsListItem"></div>
+            <div className={styles.ourProductsWrapper}>
+                <div className={styles.ourProductsDie}>
+                    <div className={styles.ourProductsDieImage}></div>
+                    <div className={styles.ourProductsDieText}>Explore Our Products</div>
+                </div>
+                <div className={styles.ouProductsTitle}></div>
+                <div className={styles.ourProductsList}>
+                    <div className={styles.ourProductsListItem}></div>
                 </div>
             </div>
         </div>
