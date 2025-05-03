47a8470ef9f5650037533990de134d4c272aa094
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-03 22:31:49 +0300

    add title

--- webpages/electronics-store/src/components/Featured/Featured.jsx
+++ webpages/electronics-store/src/components/Featured/Featured.jsx
@@ -1,16 +1,18 @@
-import styles from '../Featured/Featured.module.css';
+import styles from "../Featured/Featured.module.css";
 
-function Featured () {
-  return (
-    <div className={styles.featured}>
-        <div className={styles.featuredDie}>
-            <div className={styles.featuredDieImage}></div>
-            <div className={styles.featuredDieText}></div>
+function Featured() {
+    return (
+        <div className={styles.featured}>
+            <div className={styles.featuredWrapper}>
+                <div className={styles.featuredDie}>
+                    <div className={styles.featuredDieImage}></div>
+                    <div className={styles.featuredDieText}>Featured</div>
+                </div>
+                <div className={styles.featuredTitle}>New Arrival</div>
+                <div className={styles.featuredCards}></div>
+            </div>
         </div>
-        <div className={styles.featuredTitle}></div>
-        <div className={styles.featuredCards}></div>
-    </div>
-  )
+    );
 }
 
 export default Featured;
