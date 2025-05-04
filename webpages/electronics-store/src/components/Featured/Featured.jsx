63aad555ca78dd1519c32b9f08e55312ec241baf
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-04 23:57:48 +0300

    stylize links

--- webpages/electronics-store/src/components/Featured/Featured.jsx
+++ webpages/electronics-store/src/components/Featured/Featured.jsx
@@ -1,8 +1,8 @@
 import styles from "../Featured/Featured.module.css";
-import playstation from '/playstation.webp';
-import womens_collections from '/women’s _collections.webp';
-import wireless_speakers from '/wireless_speakers.webp';
-import gucci_parfume from '/gucci_intense_parfume.webp';
+import playstation from "/playstation.webp";
+import womens_collections from "/women’s _collections.webp";
+import wireless_speakers from "/wireless_speakers.webp";
+import gucci_parfume from "/gucci_intense_parfume.webp";
 
 function Featured() {
     return (
@@ -14,18 +14,39 @@ function Featured() {
                 </div>
                 <div className={styles.featuredTitle}>New Arrival</div>
                 <div className={styles.featuredCards}>
-                    <div className={styles.featuredCardsExample1}>
+                    <div className={styles.featuredCardFirst}>
+                            <div className={styles.featuredCardText}>
+                                <div className={styles.featuredCardTitle}>PlayStation 5</div>
+                                <div className={styles.featuredCardDetails}>Black and White version of the PS5 coming out on sale.</div>
+                                <a href="#" className={styles.featuredCardLink}>Shop Now</a>
+                            </div>
                         <img src={playstation} alt="" />
-                        </div>
-                    <div className={styles.gridContainer}>
-                    <div className={styles.featuredCardsExample2}>
-                        <img src={womens_collections} alt="" />
                     </div>
+                    <div className={styles.gridContainer}>
+                        <div className={styles.featuredCardSecond}>
+                                <div className={styles.featuredCardSecondText}>
+                                    <div className={styles.featuredCardTitle}>Women’s Collections</div>
+                                    <div className={styles.featuredCardDetails}>Featured woman collections that give you another vibe.</div>
+                                    <a href="#" className={styles.featuredCardLink}>Shop Now</a>
+                                </div>
+                            <div className={styles.featuredCardsExample2}></div>
+                            <img src={womens_collections} alt="" />
+                        </div>
                         <div className={styles.gridContainer2}>
-                            <div className={styles.featuredCardsExample3}>
+                            <div className={styles.featuredCardThird}>
+                                <div className={styles.featuredCardThirdText}>
+                                        <div className={styles.featuredCardTitle}>Speakers</div>
+                                        <div className={styles.featuredCardDetails}>Amazon wireless speakers</div>
+                                        <a href="#" className={styles.featuredCardLink}>Shop Now</a>
+                                    </div>
                                 <img src={wireless_speakers} alt="" />
                             </div>
-                            <div className={styles.featuredCardsExample4}>
+                            <div className={styles.featuredCardFourth}>
+                                    <div className={styles.featuredCardFourthText}>
+                                        <div className={styles.featuredCardTitle}>Speakers</div>
+                                        <div className={styles.featuredCardDetails}>Amazon wireless speakers</div>
+                                        <a href="#" className={styles.featuredCardLink}>Shop Now</a>
+                                    </div>
                                 <img src={gucci_parfume} alt="" />
                             </div>
                         </div>
