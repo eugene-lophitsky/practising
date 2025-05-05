74b2d05283fb99a362991d1c10333a4db2336781
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-05 22:50:40 +0300

    add feedback elements

--- webpages/electronics-store/src/components/Footer/Footer.jsx
+++ webpages/electronics-store/src/components/Footer/Footer.jsx
@@ -1,15 +1,30 @@
 import styles from '../Footer/Footer.module.css';
+import iconSend from '/icon-send.svg';
 
 function Footer () {
   return (
     <div className={styles.footer}>
         <div className={styles.footerWrapper}>
             <div className={styles.footerCommon}>
-                <div className={styles.footerFeedback}></div>
+
+                <div className={styles.footerFeedback}>
+                    <div className={styles.footerFeedbackTitle}>Exclusive</div>
+                    <div className={styles.footerFeedbackLink}>Subscribe</div>
+                    <div className={styles.footerFeedbackOffer}>Get 10% off your first order</div>
+                    <form action="" className={styles.footerFeedbackForm}>
+                        <input type="text" className={styles.footerFeedbackFormInput}/>
+                        <a className={styles.footerFeedbackFormButton}>
+                            <img src={iconSend} alt="" />
+                        </a>
+                    </form>
+                </div>
+
                 <div className={styles.footerLinks}></div>
                 <div className={styles.footerApps}></div>
             </div>
-            <div className={styles.footerCopyright}></div>
+            <div className={styles.footerCopyright}>
+            &#169 Copyright Rimel 2025. All right reserved
+            </div>
         </div>
     </div>
   )
