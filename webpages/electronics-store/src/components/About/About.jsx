97a0464302368afa5fc44e33cca820f69a97174d
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-09 23:13:55 +0300

    add text, img

--- webpages/electronics-store/src/components/About/About.jsx
+++ webpages/electronics-store/src/components/About/About.jsx
@@ -1,9 +1,30 @@
 import styles from './About.module.css';
+import jumbotronPhoto from '/about-us-jumbotron.webp';
 
 function About () {
     return (
         <div className={styles.about}>
-            <div className={styles.aboutJumbotron}></div>
+            <div className={styles.aboutJumbotron}>
+                <div className={styles.aboutJumbotronDescription}>
+                    <div className={styles.aboutJumbotronDescriptionTitle}>Our Story</div>
+                    <div className={styles.aboutJumbotronDescriptionText}>
+                        Launced in 2015, Exclusive is South Asia’s premier
+                        online shopping makterplace with an active presense
+                        in Bangladesh. Supported by wide range of tailored 
+                        marketing, data and service solutions, Exclusive 
+                        has 10,500 sallers and 300 brands and serves 3 
+                        millioons customers across the region.
+                        <p>
+                        Exclusive has more than 1 Million products to offer,
+                        growing at a very fast. Exclusive offers a diverse 
+                        assotment in categories ranging  from consumer.
+                        </p> 
+                    </div>
+                </div>
+                <div className={styles.aboutJumbotronImage}>
+                    <img src={jumbotronPhoto} alt="" />
+                </div>
+            </div>
             <div className={styles.aboutAchievements}></div>
             <div className={styles.aboutExperts}></div>
             <div className={styles.aboutServices}></div>
