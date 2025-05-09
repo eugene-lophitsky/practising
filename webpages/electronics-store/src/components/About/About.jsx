cfb22a256fc34bd9a9804bab8ece875b0bff8cba
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-09 22:55:40 +0300

    set 'about' basic structure

--- webpages/electronics-store/src/components/About/About.jsx
+++ webpages/electronics-store/src/components/About/About.jsx
@@ -1,7 +1,19 @@
+import styles from './About.module.css';
+
 function About () {
     return (
-        <div></div>
+        <div className={styles.about}>
+            <div className={styles.aboutJumbotron}></div>
+            <div className={styles.aboutAchievements}></div>
+            <div className={styles.aboutExperts}></div>
+            <div className={styles.aboutServices}></div>
+        </div>
     )
 }
 
 export default About;
+
+
+
+
+// about-us-jumbotron.webp
