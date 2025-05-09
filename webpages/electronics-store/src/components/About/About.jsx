e74cd47b273bf4943b9b7727bb34005b46dda5ca
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-09 23:35:15 +0300

    'about' is done

--- webpages/electronics-store/src/components/About/About.jsx
+++ webpages/electronics-store/src/components/About/About.jsx
@@ -6,19 +6,23 @@ function About () {
         <div className={styles.about}>
             <div className={styles.aboutJumbotron}>
                 <div className={styles.aboutJumbotronDescription}>
-                    <div className={styles.aboutJumbotronDescriptionTitle}>Our Story</div>
-                    <div className={styles.aboutJumbotronDescriptionText}>
-                        Launced in 2015, Exclusive is South Asia’s premier
-                        online shopping makterplace with an active presense
-                        in Bangladesh. Supported by wide range of tailored 
-                        marketing, data and service solutions, Exclusive 
-                        has 10,500 sallers and 300 brands and serves 3 
-                        millioons customers across the region.
-                        <p>
-                        Exclusive has more than 1 Million products to offer,
-                        growing at a very fast. Exclusive offers a diverse 
-                        assotment in categories ranging  from consumer.
-                        </p> 
+                   <div className={styles.aboutJumbotronWrapper}>
+                        <div className={styles.aboutJumbotronDescriptionTitle}>Our Story</div>
+                        <div className={styles.aboutJumbotronDescriptionText}>
+                            <p className={styles.aboutJumbotronDescriptionSection}>
+                            Launced in 2015, Exclusive is South Asia’s premier
+                            online shopping makterplace with an active presense
+                            in Bangladesh. Supported by wide range of tailored 
+                            marketing, data and service solutions, Exclusive 
+                            has 10,500 sallers and 300 brands and serves 3 
+                            millioons customers across the region.
+                            </p>
+                            <p className={styles.aboutJumbotronDescriptionSection}>
+                            Exclusive has more than 1 Million products to offer,
+                            growing at a very fast. Exclusive offers a diverse 
+                            assotment in categories ranging  from consumer.
+                            </p> 
+                        </div>
                     </div>
                 </div>
                 <div className={styles.aboutJumbotronImage}>
