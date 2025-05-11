abbd812f90e80f9c219bb7ee48356f31436f3615
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-11 21:18:04 +0300

    stylize cards

--- webpages/electronics-store/src/components/About/About.jsx
+++ webpages/electronics-store/src/components/About/About.jsx
@@ -1,35 +1,11 @@
+import Jumbotron from '../About/Jumbotron/Jumbotron.jsx';
 import styles from './About.module.css';
-import jumbotronPhoto from '/about-us-jumbotron.webp';
 import aboutCards from './aboutCards.js';
 
 function About () {
     return (
         <div className={styles.about}>
-            <div className={styles.aboutJumbotron}>
-                <div className={styles.aboutJumbotronDescription}>
-                   <div className={styles.aboutJumbotronWrapper}>
-                        <div className={styles.aboutJumbotronDescriptionTitle}>Our Story</div>
-                        <div className={styles.aboutJumbotronDescriptionText}>
-                            <p className={styles.aboutJumbotronDescriptionSection}>
-                            Launced in 2015, Exclusive is South Asia’s premier
-                            online shopping makterplace with an active presense
-                            in Bangladesh. Supported by wide range of tailored 
-                            marketing, data and service solutions, Exclusive 
-                            has 10,500 sallers and 300 brands and serves 3 
-                            millioons customers across the region.
-                            </p>
-                            <p className={styles.aboutJumbotronDescriptionSection}>
-                            Exclusive has more than 1 Million products to offer,
-                            growing at a very fast. Exclusive offers a diverse 
-                            assotment in categories ranging  from consumer.
-                            </p> 
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.aboutJumbotronImage}>
-                    <img src={jumbotronPhoto} alt="" />
-                </div>
-            </div>
+            <Jumbotron/>
             <div className={styles.aboutAchievements}>
                 {aboutCards.map(aboutCard => (
                     <div key={aboutCard.details} className={styles.aboutAchievementsCard}>
--- webpages/electronics-store/src/components/About/Jumbotron/Jumbotron.jsx
+++ webpages/electronics-store/src/components/About/Jumbotron/Jumbotron.jsx
@@ -1,2 +1,34 @@
-import styles from './Jumbotron.module.css';
+import styles from '../Jumbotron/Jumbotron.module.css';
 import jumbotronPhoto from '/about-us-jumbotron.webp';
+
+function Jumbotron () {
+  return (
+               <div className={styles.aboutJumbotron}>
+                <div className={styles.aboutJumbotronDescription}>
+                   <div className={styles.aboutJumbotronWrapper}>
+                        <div className={styles.aboutJumbotronDescriptionTitle}>Our Story</div>
+                        <div className={styles.aboutJumbotronDescriptionText}>
+                            <p className={styles.aboutJumbotronDescriptionSection}>
+                            Launced in 2015, Exclusive is South Asia’s premier
+                            online shopping makterplace with an active presense
+                            in Bangladesh. Supported by wide range of tailored 
+                            marketing, data and service solutions, Exclusive 
+                            has 10,500 sallers and 300 brands and serves 3 
+                            millioons customers across the region.
+                            </p>
+                            <p className={styles.aboutJumbotronDescriptionSection}>
+                            Exclusive has more than 1 Million products to offer,
+                            growing at a very fast. Exclusive offers a diverse 
+                            assotment in categories ranging  from consumer.
+                            </p> 
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.aboutJumbotronImage}>
+                    <img src={jumbotronPhoto} alt="" />
+                </div>
+            </div>
+  )
+}
+
+export default Jumbotron;
