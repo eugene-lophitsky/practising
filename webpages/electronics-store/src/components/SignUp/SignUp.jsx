d92b9686887c1db344ad44ed2f2758f56480a490
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-15 21:54:42 +0300

    add margins

--- webpages/electronics-store/src/components/SignUp/SignUp.jsx
+++ webpages/electronics-store/src/components/SignUp/SignUp.jsx
@@ -1,9 +1,17 @@
 import styles from './SignUp.module.css';
+import sigUpBanner from '/sign-up-banner.webp';
 
 function SignUp () {
   return (
     <div className={styles.signUp}>
-      
+      <div className={styles.signUpBanner}><img src={sigUpBanner} alt=""/></div>
+      <div className={styles.signUpForm}>
+        <div className={styles.signUpWrapper}>
+          <div className={styles.signUpTitle}>Create an account</div>
+          <div className={styles.signUpSubtitle}>Enter your details below</div>
+          <form></form>
+        </div>
+      </div>
     </div>
   )
 }
