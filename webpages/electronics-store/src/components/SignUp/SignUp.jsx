477380c9037f6197d84e462639a279b640030abd
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-16 21:12:27 +0300

    wip

--- webpages/electronics-store/src/components/SignUp/SignUp.jsx
+++ webpages/electronics-store/src/components/SignUp/SignUp.jsx
@@ -1,22 +1,11 @@
+import SignUpMain from './SignUpMain/SignUpMain';
 import styles from './SignUp.module.css';
-import sigUpBanner from '/sign-up-banner.webp';
+
 
 function SignUp () {
   return (
     <div className={styles.signUp}>
-      <div className={styles.signUpBanner}><img src={sigUpBanner} alt=""/></div>
-      <div className={styles.signUpForm}>
-        <div className={styles.signUpWrapper}>
-          <div className={styles.signUpTitle}>Create an account</div>
-          <div className={styles.signUpSubtitle}>Enter your details below</div>
-          <form className={styles.signUpForm}>
-            <input type="text" name="name" className={styles.signUpFormName} placeholder="Name" />
-            <input type="text" className={styles.signUpFormPhoneNumber} placeholder="Email or Phone Number" />
-            <input type="password" className={styles.signUpFormPassword} placeholder="Password" />
-            <a href="#" className={styles.signUpFormButton}>Create Account</a>
-          </form>
-        </div>
-      </div>
+      <SignUpMain/>
     </div>
   )
 }
---
+++ webpages/electronics-store/src/components/SignUp/SignUpMain/SignUpMain.jsx
@@ -0,0 +1,24 @@
+import styles from '../SignUpMain/SignUpMain.module.css';
+import sigUpBanner from '/sign-up-banner.webp';
+
+function SignUpMain () {
+  return (
+    <>
+        <div className={styles.signUpBanner}><img src={sigUpBanner} alt=""/></div>
+             <div className={styles.signUpForm}>
+             <div className={styles.signUpWrapper}>
+             <div className={styles.signUpTitle}>Create an account</div>
+             <div className={styles.signUpSubtitle}>Enter your details below</div>
+             <form className={styles.signUpForm}>
+             <input type="text" name="name" className={styles.signUpFormName} placeholder="Name" />
+             <input type="text" className={styles.signUpFormPhoneNumber} placeholder="Email or Phone Number" />
+             <input type="password" className={styles.signUpFormPassword} placeholder="Password" />
+             <a href="#" className={styles.signUpFormButton}>Create Account</a>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default SignUpMain;
