c07d6b13de2844086ddad2e8590d0063c4a64e7d
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-16 21:24:14 +0300

    set footer basic container

---
+++ webpages/electronics-store/src/components/SignUp/SignUpFooter/SignUpFooter.jsx
@@ -0,0 +1,9 @@
+import styles from '../SignUpFooter/SignUpFooter.module.css';
+
+function SignUpFooter () {
+  return (
+    <div className={styles.SignUpFooter}></div>
+  )
+}
+
+export default SignUpFooter;
