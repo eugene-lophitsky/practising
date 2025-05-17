8bc9d0653af1f0c09f2a10ea3bae1b4744b47a17
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-17 21:47:41 +0300

    add inputs, useState

--- interface/custom type password/src/App.jsx
+++ interface/custom type password/src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react"
+import './App.css';
 
 function App() {
 
+  const [inputType, setInputType] = useState('password');
 
   return (
-    <>
-     
-    </>
+    <div className="inputWrapper">
+         <input type={inputType} name="text" className="textfield"  />
+         <input type="checkbox" name="switcher" id="switcher" className="switcher" onChange={(event) => event.target.checked ? setInputType("text") : setInputType("password")} />
+         <label htmlFor="switcher"></label>
+    </div>
   )
 }
 
-export default App
+export default App;
