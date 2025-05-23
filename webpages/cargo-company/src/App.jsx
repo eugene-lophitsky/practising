f0c219838ca5386be663b37b5f2b05227bff1bb3
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-23 23:35:37 +0300

    wip

--- webpages/cargo-company/src/App.jsx
+++ webpages/cargo-company/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './Header/Header';
 import Jumbotron from './Jumbotron/Jumbotron.jsx';
+import PriceCalculating from './PriceCalculating/PriceCalculating.jsx';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header/>
       <Jumbotron/>
+      <PriceCalculating/>
     </>
   )
 }
