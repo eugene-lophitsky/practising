2b2790851f744c9acfd63f1e9c8d6573c30d2df9
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-05-31 22:21:35 +0300

    set 'why choose us'  basic structure

---
+++ webpages/cargo-company/src/WhyChooseUs/WhyChooseUs.jsx
@@ -0,0 +1,15 @@
+
+function WhyChooseUs () {
+  return (
+    <div className="why-choose-us">
+        <div className="why-choose-us__wrapper">
+            <div className="why-choose-us__title"></div>
+            <div className="why-choose-us__explanations">
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default WhyChooseUs;
