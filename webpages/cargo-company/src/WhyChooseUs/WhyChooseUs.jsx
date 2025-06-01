7bccad2b1d07c7b7738f73e7fe2f797567c4109b
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-01 22:54:14 +0300

    add icons

--- webpages/cargo-company/src/WhyChooseUs/WhyChooseUs.jsx
+++ webpages/cargo-company/src/WhyChooseUs/WhyChooseUs.jsx
@@ -1,18 +1,32 @@
-import data from './data.js';
+import data from "./data.js";
 
-function WhyChooseUs () {
-  return (
-    <div className="why-choose-us">
-        <div className="why-choose-us__wrapper">
-            <div className="why-choose-us__title">ПОЧЕМУ СТОИТ ВЫБРАТЬ ИМЕННО НАС?</div>
-            <div className="why-choose-us__explanations">
-                {data.map(item => (
-                  <div key={item.title}>{item.title}</div>
-                ))}
+function WhyChooseUs() {
+    return (
+        <div className="why-choose-us">
+            <div className="why-choose-us__wrapper">
+                <div className="why-choose-us__content">
+                    <div className="why-choose-us__title">
+                        ПОЧЕМУ СТОИТ ВЫБРАТЬ ИМЕННО НАС?
+                    </div>
+                    <div className="divider"></div>
+                    <div className="why-choose-us__explanations">
+                        {data.map((item) => (
+                            <div key={item.title} className="why-choose-us__explanation">
+                                <div className="why-choose-us__explanation-wrapper">
+                                    <div className="why-choose-us__explanation-icon">
+                                      <img src={item.img} alt="" />
+                                    </div>
+                                </div>
+                                <div className="why-choose-us__explanation-title">{item.title}</div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    );
 }
 
 export default WhyChooseUs;
+
+
