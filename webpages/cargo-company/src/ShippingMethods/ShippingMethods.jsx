a31b0d320f52f987ba587096134266659bbf9b6a
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-02 22:15:56 +0300

    initialize 'shipping methods'

---
+++ webpages/cargo-company/src/ShippingMethods/ShippingMethods.jsx
@@ -0,0 +1,13 @@
+
+function ShippingMethods () {
+  return (
+    <div className="shipping-methods">
+        <div className="shipping-methods__wrapper">
+            <div className="shipping-methods__title"></div>
+            <div className="shipping-methods__cards"></div>
+        </div>
+    </div>
+  )
+}
+
+export default ShippingMethods;
