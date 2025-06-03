7200629f97571b5941a2c330cde77309417b1315
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-03 23:25:25 +0300

    align cards, add gap

--- webpages/cargo-company/src/ShippingMethods/ShippingMethods.jsx
+++ webpages/cargo-company/src/ShippingMethods/ShippingMethods.jsx
@@ -9,17 +9,17 @@ function ShippingMethods () {
             <div className="shipping-methods__cards">
               {data.map(item => (
                 <div key={item.title} className="shipping-methods__card">
-                  <span className="shipping-methods__image"><img src={item.img} alt="" /></span>
+                  <span className="shipping-methods__image-wrapper"><img className="shipping-methods__image" src={item.img} alt="" /></span>
                   <div className="shipping-methods__card-title">{item.title}</div>
                   <div className="shipping-methods__params">
-                      <div className="shipping-methods__time">
+                      <span className="shipping-methods__time">
                         <img src={item.datePicker} className="shipping-methods__time-icon"/>
                         <span className="shipping-methods__time-text">{item.time}</span>
-                      </div>
-                      <div className="shipping-methods__weight">
+                      </span>
+                      <span className="shipping-methods__weight">
                         <img src={item.container} alt="" />
                         <span>{item.initWeight}</span>
-                      </div>
+                      </span>
                       <div className="shipping-methods__price">
                         <span className="shipping-methods__price-initValue">{item.price.initValue}</span>
                         <span className="shipping-methods__price-amount">{item.price.amount}</span>
