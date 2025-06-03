a73af95f0a00cbe57a5d8bf87d094f92141dd42b
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-03 23:18:48 +0300

    add text, icons

---
+++ webpages/cargo-company/src/ShippingMethods/ShippingMethods.jsx
@@ -0,0 +1,38 @@
+import data from "./data";
+
+function ShippingMethods () {
+  return (
+    <div className="shipping-methods">
+        <div className="shipping-methods__wrapper">
+            <div className="shipping-methods__title">варианты доставки грузов из Китая</div>
+            <div className="shipping-methods__divider"></div>
+            <div className="shipping-methods__cards">
+              {data.map(item => (
+                <div key={item.title} className="shipping-methods__card">
+                  <span className="shipping-methods__image"><img src={item.img} alt="" /></span>
+                  <div className="shipping-methods__card-title">{item.title}</div>
+                  <div className="shipping-methods__params">
+                      <div className="shipping-methods__time">
+                        <img src={item.datePicker} className="shipping-methods__time-icon"/>
+                        <span className="shipping-methods__time-text">{item.time}</span>
+                      </div>
+                      <div className="shipping-methods__weight">
+                        <img src={item.container} alt="" />
+                        <span>{item.initWeight}</span>
+                      </div>
+                      <div className="shipping-methods__price">
+                        <span className="shipping-methods__price-initValue">{item.price.initValue}</span>
+                        <span className="shipping-methods__price-amount">{item.price.amount}</span>
+                        <span className="shipping-methods__price-currency">{item.price.currency}</span>
+                        <span className="shipping-methods__price-weightUnit">{item.price.weightUnit}</span>
+                      </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ShippingMethods;
--- webpages/cargo-company/src/ShippingMethods/data.js
+++ webpages/cargo-company/src/ShippingMethods/data.js
@@ -1,7 +1,46 @@
-const data =  [
- {img: '/shipping-methods_img1.webp', title: 'Авто - обычная', datePicker: '/calendar.svg', container: '/box.svg', 
-    price: {before: 'от', amount: '2,5', currency: 'CNY/кг'}
-}
-]
+const data = [
+    {
+        img: "/shipping-methods_img1.webp",
+        title: "Авто - обычная",
+        datePicker: "/calendar.svg",
+        time: "9-15 дней",
+        container: "/box.svg",
+        initWeight: "от 5кг",
+        price: {
+            initValue: "от",
+            amount: "2,5",
+            currency: "CNY",
+            weightUnit: "кг",
+        },
+    },
+    {
+        img: "/shipping-methods_img2.webp",
+        title: "Авто - ускоренная",
+        datePicker: "/calendar.svg",
+        time: "9-15 дней",
+        container: "/box.svg",
+        initWeight: "от 5кг",
+        price: {
+            initValue: "от",
+            amount: "2,5",
+            currency: "CNY",
+            weightUnit: "кг",
+        },
+    },
+    {
+        img: "/shipping-methods_img3.webp",
+        title: "Авиа",
+        datePicker: "/calendar.svg",
+        time: "9-15 дней",
+        container: "/box.svg",
+        initWeight: "от 5кг",
+        price: {
+            initValue: "от",
+            amount: "2,5",
+            currency: "CNY",
+            weightUnit: "кг",
+        },
+    },
+];
 
 export default data;
