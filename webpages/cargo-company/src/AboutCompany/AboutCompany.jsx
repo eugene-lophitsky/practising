1b6961ed25fc1c0996de373d3bcf18d9d5523d11
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-10 21:56:12 +0300

    set 'about company' structure

---
+++ webpages/cargo-company/src/AboutCompany/AboutCompany.jsx
@@ -0,0 +1,13 @@
+
+function AboutCompany () {
+  return (
+    <div className="about-company">
+        <div className="about-company__wrapper">
+            <div className="about-company__info"></div>
+            <div className="about-company__image"></div>
+        </div>
+    </div>
+  )
+}
+
+export default AboutCompany;
