39a086c44b060c3c0762734c93f83cc2efe62489
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-16 22:25:23 +0300

    inialize 'cooperation' basic structure

---
+++ webpages/cargo-company/src/Cooperation/Cooperation.jsx
@@ -0,0 +1,13 @@
+
+function Cooperation () {
+  return (
+    <div className="cooperation">
+        <div className="cooperation__wrapper">
+            <div className="cooperation__title"></div>
+            <div className="cooperation-body"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Cooperation;
