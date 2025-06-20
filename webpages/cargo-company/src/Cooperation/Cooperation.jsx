f8e6a6086a6ac586cb216466f738d892520b70bd
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-20 22:42:17 +0300

    include images

--- webpages/cargo-company/src/Cooperation/Cooperation.jsx
+++ webpages/cargo-company/src/Cooperation/Cooperation.jsx
@@ -1,3 +1,6 @@
+import cooperation_img1 from '/cooperation_image1.webp';
+import cooperation_img2 from '/cooperation_image2.webp';
+
 
 function Cooperation () {
   return (
@@ -6,8 +9,14 @@ function Cooperation () {
             <div className="cooperation__title">готовы к сотрудничеству?</div>
             <div className="cooperation__subtitle">Зарегистрируйте груз!</div>
             <div className="cooperation-body">
-              <div className="cooperation-info"></div>
-              <div className="cooperation-info"></div>
+              <div className="cooperation-info">
+                <div className="cooperation-info__text"></div>
+                <div className="cooperation-info__image"></div>
+              </div>
+              <div className="cooperation-info">
+                <div className="cooperation-info__text"></div>
+                <div className="cooperation-info__image"></div>
+              </div>
             </div>
         </div>
     </div>
