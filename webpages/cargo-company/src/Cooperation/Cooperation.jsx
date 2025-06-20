cb09b0270a7c30d6d5f6ded825f5c79c196810fe
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-20 23:10:59 +0300

    set margins, paddings

--- webpages/cargo-company/src/Cooperation/Cooperation.jsx
+++ webpages/cargo-company/src/Cooperation/Cooperation.jsx
@@ -4,10 +4,11 @@ import cooperation_img2 from '/cooperation_image2.webp';
 
 function Cooperation () {
   return (
-    <div className="cooperation">
+    <div className="cooperation cooperation--width">
         <div className="cooperation__wrapper">
             <div className="cooperation__title">готовы к сотрудничеству?</div>
             <div className="cooperation__subtitle">Зарегистрируйте груз!</div>
+            <div className="cooperation__divider"></div>
             <div className="cooperation-body">
               <div className="cooperation-info">
                 <div className="cooperation-info__text">
@@ -35,12 +36,12 @@ function Cooperation () {
                   </p>
                 </div>
                 <div className="cooperation-info__image">
-                    <img src={cooperation_img1} alt="" />
+                    <img src={cooperation_img2} alt="" />
                 </div>
               </div>
               <div className="cooperation-info">
                 <div className="cooperation-info__image">
-                    <img src={cooperation_img2} alt="" />
+                    <img src={cooperation_img1} alt="" />
                 </div>
                 <div className="cooperation-info__text">
                   <p className="cooperation-info__text-paragraph">
