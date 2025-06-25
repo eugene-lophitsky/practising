0a5476a1d1a006b625f9c6ddfe6d079cdace64b7
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-06-25 22:16:15 +0300

    wip

--- webpages/cargo-company/src/Header/Header.jsx
+++ webpages/cargo-company/src/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import logo from '/header-logo.png';
 import burgerMenuClosed from '/burger-menu-closed.png';
+import burgerMenuOpened from '/burger-menu-opened.png';
 
 function Header () {
 
@@ -26,26 +27,9 @@ const [burgerMenu, setBurgerMenu] = useState();
                </ul>
             </nav>
             <a href="#" className="header__button">Оставить заявку</a>
-
             <div className="burger-menu" onClick={newButton}>
-                <img src={burgerMenu ? burgerMenuClosed : burgerMenuClosed} alt="" />
+                <img src={burgerMenu ? burgerMenuOpened : burgerMenuClosed} alt="" />
             </div>
-
-
-
-                    {/* <div className="menu">
-                      <input type="checkbox" id="burger-checkbox" className="burger-checkbox" />
-                      <label htmlFor="burger-checkbox" className="burger"></label>
-                      <ul className="menu-list">
-                      <li><a href="#" className="menu-item">Home</a></li>
-                      <li></li>
-                      <li><a href="#" className="menu-item">Package</a></li>
-                      <li></li>
-                      <li><a href="#" className="menu-item">Destination</a></li>
-                      <li><a href="#" className="menu-item">Blog</a></li>
-                      <li><a href="#" className="menu-item">Contact</a></li>
-                      </ul>
-                    </div> */}
         </div>
     </div>
   )
