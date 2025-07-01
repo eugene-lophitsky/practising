7f40d4b0399d475978521a0fb2d858bf7741b290
eugene-lophitsky <eugene-lophitsky@yandex.ru>
2025-07-01 21:16:11 +0300

    adding fn to button

--- experiments_on_TS/server/app.js
+++ experiments_on_TS/server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const responseData =  require('./responseData.json');
+app.use(cors());
 
 app.get('/', (request, response) => {
     response.json(responseData)
--- experiments_on_TS/src/Button.tsx
+++ experiments_on_TS/src/Button.tsx
@@ -11,9 +11,16 @@ function Button ({size, variant, disabled, children}: ButtonProps) {
 
   const [counter, setCounter] = useState<number>(0);
 
+ function getIt () {
+   fetch('http://localhost:3000/')
+  .then(response => response.json())
+  .then(data => console.log(data));
+  setCounter(counter+1);
+ }
+
   return (
    <div className="button-wrapper">
-     <button className="button" size={size} disabled={disabled} variant={variant} onClick={()=> setCounter(counter+1)}>{children} {counter > 0 ?  `нажата ${counter} ${"раз"}` : ""}</button>
+     <button className="button" size={size} disabled={disabled} variant={variant} onClick={getIt}>{children} {counter > 0 ?  `нажата ${counter} ${"раз"}` : ""}</button>
    </div>
   )
 }
